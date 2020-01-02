26767018273a5de6bab94cf6d45f86292ce2b0a8
lancerdonnie <jideadedejifirst@gmail.com>
2020-01-02 09:25:15 +0100

    created server

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+//workers
+//get user private /api/user
+
+//login user /api/auth
+
+//get clients private /api/clients
+
+//admin
+//register client private  /api/clients
+
+//delete client private /api/clients
+
+//
