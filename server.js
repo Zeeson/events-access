f1bbc3f200d244511dfbb2c6977c5ed969a57080
lancerdonnie <jideadedejifirst@gmail.com>
2020-01-20 12:40:23 +0100

    configured for heroku

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const DB = require('./config/db');
 const routes = require('./routes/routes');
 const admin = require('./routes/admin');
+const path = require('path');
+
 app.use(express.json());
 DB();
 
@@ -11,6 +13,13 @@ app.get('/api', (req, res) => {
 });
 app.use('/api', routes);
 app.use('/api/admin', admin);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
